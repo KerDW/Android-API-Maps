831f4d87b2e60d145658faf1d0fa559761556daa
Álvaro Zamora <alvarozajim@gmail.com>
2020-02-21 16:25:20 +0100

    websockets connecting

--- app/src/main/java/com/example/apitest/MainActivity.java
+++ app/src/main/java/com/example/apitest/MainActivity.java
@@ -2,11 +2,20 @@ package com.example.apitest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.widget.Toast;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -63,5 +72,55 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("xd", "Network Error :: " + t.getLocalizedMessage());
             }
         });
+
+        SocketClient socks = null;
+        try {
+            socks = new SocketClient(new URI("ws://172.16.12.12:6001/app/undostres"));
+            Log.i("xd", socks.getURI().toString());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        socks.connect();
+
     }
+
+    public static class SocketClient extends WebSocketClient {
+
+        public SocketClient( URI serverUri , Draft draft ) {
+            super( serverUri, draft );
+        }
+
+        public SocketClient( URI serverURI ) {
+            super( serverURI );
+        }
+
+        public SocketClient( URI serverUri, Map<String, String> httpHeaders ) {
+            super(serverUri, httpHeaders);
+        }
+
+        @Override
+        public void onOpen( ServerHandshake handshakedata ) {
+            send("{\"event\": \"xd\"}");
+            Log.i("xd", "opened connection" );
+            // if you plan to refuse connection based on ip or httpfields overload: onWebsocketHandshakeReceivedAsClient
+        }
+
+        @Override
+        public void onMessage( String message ) {
+            Log.i("xd",  "received: " + message );
+        }
+
+        @Override
+        public void onClose( int code, String reason, boolean remote ) {
+            // The codecodes are documented in class org.java_websocket.framing.CloseFrame
+            Log.i("xd", "Connection closed by " + ( remote ? "remote peer" : "us" ) + " Code: " + code + " Reason: " + reason );
+        }
+
+        @Override
+        public void onError( Exception ex ) {
+            Log.i("xd", ex.toString());
+            // if the error is fatal then onClose will be called additionally
+        }
+    }
+
 }
