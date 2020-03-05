9ccca8852c387a528239b181a4d1f0ec2788709c
Álvaro Zamora <alvarozajim@gmail.com>
2020-03-05 19:07:55 +0100

    socket io connection working

--- app/src/main/java/com/example/apitest/MainActivity.java
+++ app/src/main/java/com/example/apitest/MainActivity.java
@@ -11,7 +11,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.net.URI;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
 import java.net.URISyntaxException;
 import java.util.List;
 
@@ -24,6 +26,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     ApiService service;
+    Socket mSocket;
 
     EditText message;
     TextView messageReceived;
@@ -37,46 +40,69 @@ public class MainActivity extends AppCompatActivity {
         message = findViewById(R.id.message);
         messageReceived = findViewById(R.id.messageReceived);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://"+getString(R.string.ip)+":80/laravelrestapi/public/api/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        try {
+            mSocket = IO.socket("http://"+getString(R.string.ip)+":5000");
+            mSocket.connect();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            Log.e("xd", e.getMessage()+"xd");
+        }
 
-        service = retrofit.create(ApiService.class);
 
-        Call<List<Car>> callAsync = service.getCars();
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl("http://"+getString(R.string.ip)+":80/laravelrestapi/public/api/")
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
+//
+//        service = retrofit.create(ApiService.class);
+//
+//        Call<List<Car>> callAsync = service.getCars();
+//
+//        Log.e("xd",callAsync.request().url().toString());
+//
+//        callAsync.enqueue(new Callback<List<Car>>()
+//        {
+//            @Override
+//            public void onResponse(Call<List<Car>> call, Response<List<Car>> response)
+//            {
+//                if (response.body() != null)
+//                {
+//                    List<Car> cars = response.body();
+//
+//                    for (Car car : cars) {
+////                        Log.e("xd",car.toString());
+//                    }
+//
+//                } else
+//                {
+//                    System.out.println("Request Error :: " + response.errorBody());
+//                    Toast.makeText(
+//                            getApplicationContext(),
+//                            "Name not found.",
+//                            Toast.LENGTH_LONG).show();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<Car>> call, Throwable t)
+//            {
+//                Log.e("xd", "Network Error :: " + t.getLocalizedMessage());
+//            }
+//        });
 
-        Log.e("xd",callAsync.request().url().toString());
+    }
 
-        callAsync.enqueue(new Callback<List<Car>>()
-        {
-            @Override
-            public void onResponse(Call<List<Car>> call, Response<List<Car>> response)
-            {
-                if (response.body() != null)
-                {
-                    List<Car> cars = response.body();
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
 
-                    for (Car car : cars) {
-//                        Log.e("xd",car.toString());
-                    }
+        mSocket.disconnect();
+    }
 
-                } else
-                {
-                    System.out.println("Request Error :: " + response.errorBody());
-                    Toast.makeText(
-                            getApplicationContext(),
-                            "Name not found.",
-                            Toast.LENGTH_LONG).show();
-                }
-            }
+    public void sendMessage(View view) {
 
-            @Override
-            public void onFailure(Call<List<Car>> call, Throwable t)
-            {
-                Log.e("xd", "Network Error :: " + t.getLocalizedMessage());
-            }
-        });
+        Log.i("xd","sending message");
+        mSocket.emit("message",message.getText().toString());
 
     }
 }
