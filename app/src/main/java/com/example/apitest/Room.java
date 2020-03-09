3a4b5b565eacefccf6aa9a9c4d06a3461d7a41c6
Álvaro Zamora <alvarozajim@gmail.com>
2020-03-09 20:41:32 +0100

    add user_count attr to room

--- app/src/main/java/com/example/apitest/Room.java
+++ app/src/main/java/com/example/apitest/Room.java
@@ -4,13 +4,23 @@ public class Room {
 
     private String name;
     private int capacity;
+    private int user_count;
 
-    public Room(String name, int capacity) {
+    public Room(String name, int capacity, int user_count) {
         this.name = name;
         this.capacity = capacity;
+        this.user_count = user_count;
     }
 
     public Room() {
+    }
+
+    public int getUser_count() {
+        return user_count;
+    }
+
+    public void setUser_count(int user_count) {
+        this.user_count = user_count;
     }
 
     public String getName() {
@@ -34,6 +44,7 @@ public class Room {
         return "Room{" +
                 "name='" + name + '\'' +
                 ", capacity=" + capacity +
+                ", user_count=" + user_count +
                 '}';
     }
 }
