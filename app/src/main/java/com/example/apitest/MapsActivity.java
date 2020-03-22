b28bf3e88ec96bf973c4bfa949401f66d341fe9d
Álvaro Zamora <alvarozajim@gmail.com>
2020-03-22 12:53:56 +0100

    map timer textview and working and announcement textview

--- app/src/main/java/com/example/apitest/MapsActivity.java
+++ app/src/main/java/com/example/apitest/MapsActivity.java
@@ -5,7 +5,10 @@ import androidx.fragment.app.FragmentActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +25,14 @@ import static com.example.apitest.UserActivity.mSocket;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    String username;
+
+    CountDownTimer cdn;
+    int timeLeft;
+
+    TextView announcement;
+    TextView timerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +60,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        String username = getIntent().getStringExtra("USERNAME");
+
+        username = getIntent().getStringExtra("USERNAME");
+
+        announcement = findViewById(R.id.announcementTitle);
+        timerText = findViewById(R.id.timerText);
+
+        createTimer();
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
@@ -68,9 +85,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // update view on ui thread
             runOnUiThread(new Runnable(){
                 public void run(){
-                    mMap.addMarker(new MarkerOptions().position(location).title(markerUsername).icon(color));
+                    Marker m = mMap.addMarker(new MarkerOptions().position(location).title(markerUsername).icon(color));
+                    m.showInfoWindow();
                 }
             });
         });
+    }
+
+    public void createTimer(){
+        cdn = new CountDownTimer(15000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timeLeft = (int) millisUntilFinished;
+                timerText.setText("Time left: \n" + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Time's up!",
+                        Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MapsActivity.this, RoomsActivity.class);
+                intent.putExtra("USERNAME", username);
+
+                startActivity(intent);
+            }
+        }.start();
     }
 }
